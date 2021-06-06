061706f5d8fa0ace4ecbeac9f0daae77a37561f4
Maxim Lobas <maxoneuiux@gmail.com>
2021-06-06 07:06:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,41 +6,64 @@ const player1 = {
   weapon: ['katana'],
 
   attack: function () {
-    console.log(name + 'fight ' + name + 'Fight...');
+    console.log(player1.name + 'fight ' + player1.name + 'Fight...');
   }
 
 };
 
+const player2 = {
+
+  name: 'Sonya',
+  hp: 1000,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+  weapon: ['katana'],
+
+  attack: function () {
+    console.log(player2.name + 'fight ' + player2.name + 'Fight...');
+  }
+
+};
+
+
 function createPlayer(player, name, hp) {
 
-    const $player1 = document.createElement('div');
-    $player1.classList.add('player1');
+  const $player1 = document.createElement('div');
+  $player1.classList.add(player1);
 
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add('progressbar');
-    $player1.appendChild($progressbar);
+  const $player2 = document.createElement('div');
+  $player2.classList.add(player2);
 
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-    $player1.appendChild($character);
+  const $progressbar = document.createElement('div');
+  $progressbar.classList.add('progressbar');
+  $player1.appendChild($progressbar);
+  $player2.appendChild($progressbar);
 
-    const $img = document.createElement('img');
-    $img.src = player1.img;
-    $character.appendChild($img);
-    
-    const $life = document.createElement('div');
-    $life.classList.add('life');
-    $progressbar.appendChild($life);
-    $life.style.width = '100%';
+  const $character = document.createElement('div');
+  $character.classList.add('character');
+  $player1.appendChild($character);
+  $player2.appendChild($character);
 
-    const $name = document.createElement('div');
-    $name.classList.add('name');
-    $progressbar.appendChild($name);
-    $name.innerText = player1.name;
+  const $img = document.createElement('img');
+  $img.src = player1.img, player2.img;
+  $character.appendChild($img);
 
-    return player, name, hp;
-    
+  const $life = document.createElement('div');
+  $life.classList.add('life');
+  $progressbar.appendChild($life);
+  $life.style.width = '100';
+
+  const $name = document.createElement('div');
+  $name.classList.add('name');
+  $progressbar.appendChild($name);
+  $name.innerText = name;
+
+  return player, name, hp;
 }
 
-createPlayer('player1', 'SCORPION', 50);
-createPlayer('player2', 'SUB-ZERO', 80);
+const $player1 = createPlayer('player1', 'SCORPION', 50);
+const $player2 = createPlayer('player2', 'SUB-ZERO', 80);
+
+
+const $arenas = document.querySelector('.arenas');
+$arenas.appendChild($player1);
+$arenas.appendChild($player2);
